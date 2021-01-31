88132425326975427ad2822aae21df01caf0a2ad
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-31 13:43:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
